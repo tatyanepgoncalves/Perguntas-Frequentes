42f062754b801b2f55f47fbcf2085bb6df3817d1
Tatyane Gonçalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-02-22 21:07:57 -0300

    Perguntas Frequentes

---
+++ script.js
@@ -0,0 +1,21 @@
+const accordionBtn = document.querySelectorAll('.accordion-btn');
+
+accordionBtn.forEach(btn => {
+    btn.addEventListener('click', function() {
+        this.classList.toggle('active')
+        const accordionDescription = this.nextElementSibling
+        const plusicon = this.querySelector('.plus-icon')
+        const minusicon = this.querySelector('.minus-icon')
+
+
+        if(accordionDescription.style.maxHeight){
+            accordionDescription.style.maxHeight = null
+            plusicon.style.display = 'block'
+            minusicon.style.display = 'none'
+        } else{
+            accordionDescription.style.maxHeight = accordionDescription.scrollHeight + 'px'
+            plusicon.style.display = 'none'
+            minusicon.style.display = 'block'
+        }
+    })
+});
